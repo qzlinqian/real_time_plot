9a8aba480d6ff6ab286bc9ecdcd7f49eb3ea20c5
qzlinqian <qzlinqian@126.com>
2018-05-15 23:42:23 +0800

    success in rectangle & input from random

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# real_time_plot
-plot real time figure (in polar coordinate)
+# real time plot
+plot real time figure
+
+
+* **plot.py**  show 100 random points in [0,1)×[0,1), when y<0.5, it turns red
+* **sample.py** [example code](https://matplotlib.org/examples/animation/rain.html) (rain.py) from docs
--- plot.py
+++ plot.py
@@ -1,23 +1,42 @@
-import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
-import numpy as np
-import math
+# plot from random in rectangular coordinate
 
-plt.close()
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# plt.close()
 # clf() 清图
 # cla() 清坐标轴
 # close() # 关窗口
-plt.ion()
+# plt.ion()
 fig = plt.figure(figsize=(5, 5))
-ax = fig.add_subplot(1, 1, 1, projection='polar')
-unit = 2 * math.pi / 100
-degree = list()
-length = list()
+ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+ax.set_xlim(0, 1), ax.set_xticks([])
+ax.set_ylim(0, 1), ax.set_yticks([])
+# unit = 2 * np.pi / 100
+n = 100
+pos = np.zeros(n, dtype=[('position', float, 2),
+                         ('color', str, 1)])
+pos['position'] = np.random.uniform(0, 1, (n, 2))
+pos['color'] = 'r'
+scat = ax.scatter(pos['position'][:, 0], pos['position'][:, 1], s=30, c=pos['color'])
 
-while 1:
-    a = int(input())
-    b = int(input())
-    degree.append(a)
-    length.append(b)
-    plt.polar(a * unit, b)
-    plt.pause(0.001)
+
+def update(frame_number):
+    # Get an index which we can use to re-spawn the oldest raindrop.
+    current_index = frame_number % n
+
+    pos['position'][current_index] = np.random.uniform(0, 1, 2)  # position update
+    # color update
+    if pos['position'][current_index, 1] < 0.5:
+        pos['color'][current_index] = 'r'
+    else:
+        pos['color'][current_index] = 'b'
+
+    # Update the scatter collection, with the new colors, sizes and positions.
+    scat.set_offsets(pos['position'])
+    scat.set_color(pos['color'])
+
+
+animation = FuncAnimation(fig, update, interval=10)
+plt.show()
